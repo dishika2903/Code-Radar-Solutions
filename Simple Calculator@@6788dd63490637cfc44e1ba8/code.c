d469fc56e4494bc9594c407d1d2b852aa1f8f7a1
dishika2903 <dishika0135.becse24@chitkara.edu.in>
2025-02-16 16:58:56 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,16 +4,16 @@ int main() {
     int a,b;
     char o;
     scanf("%d %d %c", &a,&b,&o);
-    if (o=="+") {
+    if (o=='+') {
         printf("%d", a+b);
     }
-    else if (o=="-") {
+    else if (o=='-') {
         printf("%d", a-b);
     }
-    else if (o=="*") {
+    else if (o=='*') {
         printf("%d", a*b);
     }
-    else if (o=="/") {
+    else if (o=='/') {
         printf("%d", a/b);
     }
     else {
